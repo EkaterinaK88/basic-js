a9881680ced128bbf423f79dc685ea7f7d8eb5cb
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-06 12:03:17 +0400

    feat: implement function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,22 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = [];
+   
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      for (let j = 0; j < members[i].length; j++) {
+        if (members[i][j] !== ' ') {
+          res.push(members[i].slice(j,j + 1).toUpperCase());
+          break;
+        }
+      }
+    }
+  }
+
+  res = res.sort().join('');
+  
+  if (res == []) {
+    res = false;
+  }
+  
+  return res;
 };
