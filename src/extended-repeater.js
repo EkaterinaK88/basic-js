dc1d61dcb83fc9a3f90819ab2abf7ca911129079
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-06 12:04:55 +0400

    feat: implement function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,34 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  
+  let string = String(str);
+  let addition;
+  if (options.addition === null) {
+    addition = 'null';
+  } else if (options.addition === undefined) {
+    addition = '';
+  } else {addition = String(options.addition)};
+  let repeatTimes = options.repeatTimes || 1;
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let separator = options.separator || '+';
+  let additionSeparator = options.additionSeparator || '|';
+  
+  let res = '';
+  let arr =[];
+  let n = repeatTimes + repeatTimes * additionRepeatTimes;
+  for (let i = 0; i < n; i+= additionRepeatTimes + 1) {
+    arr[0] = string;
+    if (repeatTimes > 1) {
+      arr[i] = separator + string;
+    } 
+    if (additionRepeatTimes < 2) {
+      arr[i + 1] = addition;
+    } 
+    for (let j = 2; j <= additionRepeatTimes; j++) {
+      arr[i + 1] = addition;
+      arr[i + j] = additionSeparator + addition;
+    }
+  }
+  res = arr.join('');
+  return res;
 };
   
