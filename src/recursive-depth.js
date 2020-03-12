907ffe9730910c861dc6299fbd821ba1587834eb
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-12 11:48:56 +0400

    feat: implement function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,16 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  calculateDepth(array) {
+    let depth = 1;
+    let arr = [];
+      array = array.filter((i) => Array.isArray(i));
+      if (array.length != 0) { 
+        depth++;
+        for (let i = 0; i < array.length; i++) {
+          arr.push(...array[i]);
+        };
+        depth += calculateDepth(arr);
+        depth--;
+      };
+     return depth;
+  }
 };
