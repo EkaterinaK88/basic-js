55034574536da9b9d3ab251ea7d7b984239d282b
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-12 23:00:06 +0400

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,7 +5,7 @@ module.exports = function dateSample(sampleActivity) {
   let year;
   let k = 0.693/HALF_LIFE_PERIOD;
   if (typeof sampleActivity != 'string' || parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) >  
-15 || isNaN(Number(sampleActivity))) {
+15 || isNaN(parseFloat(sampleActivity))) {
     year = false;
   } else {
     year = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/k);
