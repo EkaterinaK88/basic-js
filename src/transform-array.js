e436bddff823d1a035502755902f525d4a7c05a5
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-16 23:10:43 +0400

    fix: change work of action flags

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,30 +3,41 @@ module.exports = function transform(arr) {
   for (let i = 0; i < arr.length; i++) {
     switch(arr[i]) {
       case '--double-next':
-        if (arr[i + 1]) {
+        if (String(arr[i + 1]) !== 'undefined') {
         arr.splice(i, 1, arr[i + 1]);
+        i--;
         } else {
           arr.splice(i, 1);
         }
         break;
       case '--double-prev':
-        if (arr[i - 1]) {
+        if (String(arr[i - 1]) !== 'undefined') {
         arr.splice(i, 1, arr[i - 1]);
+        
         } else {
           arr.splice(i, 1);
+          i--;
         }
         break;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
       case '--discard-prev':
-        if (arr[i - 1]) {
+        if (String(arr[i - 1]) !== 'undefined') {
         arr.splice(i - 1, 2);
+        i -= 2;
         } else {
           arr.splice(i, 1);
+          i--;
         }
         break;
       case '--discard-next':
         arr.splice(i, 2);
+        i -=2;
       break;
     } 
   }
   return arr;
 };
+
