883998053204519aafae531935371e724419d811
EkaterinaK88 <55290157+EkaterinaK88@users.noreply.github.com>
2020-03-16 23:06:45 +0400

    fix: add wrong type handling

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,24 @@
 module.exports = function createDreamTeam(members) {
   let res = [];
-   
-  for (let i = 0; i < members.length; i++) {
-    if (typeof members[i] === "string") {
-      for (let j = 0; j < members[i].length; j++) {
-        if (members[i][j] !== ' ') {
-          res.push(members[i].slice(j,j + 1).toUpperCase());
-          break;
-        }
-      }
-    }
-  }
-
-  res = res.sort().join('');
-  
-  if (res == []) {
+  if (!Array.isArray(members)) {
     res = false;
+  } else {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") {
+        for (let j = 0; j < members[i].length; j++) {
+          if (members[i][j] !== ' ') {
+            res.push(members[i].slice(j,j + 1).toUpperCase());
+            break;
+          }
+        }
+      } 
+    }
+
+    res = res.sort().join('');
+  
+    if (res == []) {
+      res = false;
+    }
   }
   
   return res;
